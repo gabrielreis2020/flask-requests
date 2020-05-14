7c5bd4e26133844cc407eb94c43ba5ef4f711f85
Gabriel Marques <gabrielreismarques123@gmail.com>
2020-05-14 20:41:59 -0300

    PythonAnyware

--- app.py
+++ app.py
@@ -21,19 +21,26 @@ def pokemon(id):
     poke = pokemons[id]
     return render_template(
         'pokemon.html',
-        pokemon=poke
+        pokemon=poke,
+        id=id
     )
+
 @app.route('/add')
 def add():
     return render_template('add.html')
 
 @app.route('/store', methods=['POST'])
 def store():
- nome = request.form['nome']
- img = request.form['img']
- 
- pokemons.append([nome, img])
- return redirect('/')
+    nome = request.form['nome']
+    img = request.form['img']
+
+    pokemons.append([nome, img])
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    del pokemons[id]
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
